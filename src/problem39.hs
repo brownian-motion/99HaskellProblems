b311164f8add879de2ca7fc8ca0c959f5f2d687f
JJ Brown <jack.brown.255@gmail.com>
2018-02-28 18:00:23 -0600

    Solved 39

--- src/problem39.hs
+++ src/problem39.hs
@@ -1,5 +1,4 @@
 module Main(main) where
-import           Data.List  (group)
 import           Test.HUnit
 
 -- Problem 39:
@@ -9,19 +8,6 @@ import           Test.HUnit
 primesInRange :: Integer -> Integer -> [Integer]
 primesInRange x y = filter isPrime [x..y]
 
--- Problem 35:
--- Determine the prime factors of a given positive integer. Construct a flat list containing the prime factors in ascending order.
-
-primeFactors :: Integer -> [Integer]
-primeFactors 1 = []
-primeFactors x | null candidatePrimes   = [x]
-               | otherwise              = firstPrimeFactor : primeFactors (x `div` firstPrimeFactor)
-                 where candidatePrimes  = dropWhile (not . (x `divisibleBy`)) (takeWhile (< x) primes)
-                       firstPrimeFactor = head candidatePrimes
-
-primes :: [Integer]
-primes = filter isPrime [2..]
-
 -- Problem 31:
 -- Determine whether a given integer number is prime.
 
@@ -30,18 +16,13 @@ isPrime n | n < 2     = False
           | n == 2    = True
           | n == 3    = True
           | otherwise = and [not (n `divisibleBy` p) | p <- candidates]
-          where candidates  = [2..upperBound]
-                upperBound  = ceiling $ sqrt $ fromInteger n
-
-divisibleBy :: (Integral a) => a -> a -> Bool
-divisibleBy x y = (x `mod` y) == 0
+          where candidates      = [2..upperBound]
+                upperBound      = ceiling $ sqrt $ fromInteger n
+                divisibleBy x y = (x `mod` y) == 0
 
 tests :: Test
 tests = TestList [ --TODO: fix these unit tests
-    TestCase (assertEqual "prime factorization of 10" [(2, 1), (5, 1)] (primeFactorsMult 10)),
-    TestCase (assertEqual "prime factorization of 45" [(3, 2), (5, 1)] (primeFactorsMult 45)),
-    TestCase (assertEqual "prime factorization of 315" [(3, 2), (5, 1), (7, 1)] (primeFactorsMult 315)),
-    TestCase (assertEqual "prime factorization of 3150" [(2, 1), (3, 2), (5, 2), (7, 1)] (primeFactorsMult 3150))
+    TestCase (assertEqual "primes from 10 to 20" [11, 13, 17, 19] (primesInRange 10 20))
     ]
 
 main :: IO Counts
